207bc2981abd6d4537e7da6dff5c7e7511c335ea
Sajjad Qayyum <sqayyum425@gmail.com>
2024-09-23 11:13:43 +0500

    Update olympics_preprocess.py with new changes

---
+++ olympics_preprocess.py
@@ -0,0 +1,15 @@
+import pandas as pd
+
+def preprocess(df, region_df):
+    # Filter data to summer Olympics
+    df = df[df['Season'] == "Summer"]
+
+    # Merge athlete data with region data based on NOC
+    df = df.merge(region_df, on='NOC', how='left')
+
+    # Remove duplicate rows
+    df.drop_duplicates(inplace=True)
+
+    # Create one-hot encoded columns for medal types
+    df = pd.concat([df, pd.get_dummies(df['Medal'])], axis=1)
+    return df
