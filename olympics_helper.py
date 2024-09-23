16a5962efbf337e520324a36ee028beb430615aa
Sajjad Qayyum <sqayyum425@gmail.com>
2024-09-23 11:10:19 +0500

    Update olympics_helper.py with new changes

---
+++ olympics_helper.py
@@ -0,0 +1,50 @@
+import numpy as np
+
+def fetch_medal_tally(df,year,country):
+    medal_df = df.drop_duplicates(subset=['Team','NOC','Games','Year','City','Sport','Event','Medal'])
+    flag = 0
+    if year == 'Overall' and country == 'Overall':
+        temp_df = medal_df
+    if year == 'Overall' and country != 'Overall':
+        flag = 1
+        temp_df = medal_df[medal_df['region'] == country]
+    if year != 'Overall' and country == 'Overall':
+        temp_df = medal_df[medal_df['Year'] == int(year)]
+    if year != 'Overall' and country != 'Overall':
+        temp_df = medal_df[(medal_df['Year'] == int(year)) & (medal_df['region'] == country)]
+
+    if flag == 1:
+        x = temp_df.groupby('Year').sum()[['Gold','Silver','Bronze']].sort_values('Year').reset_index()
+    else:
+        x = temp_df.groupby('region').sum()[['Gold','Silver','Bronze']].sort_values('Gold',ascending=False).reset_index()
+        
+    x['Total Sum'] = x['Gold'] + x['Silver'] + x['Bronze']
+    x[['Gold', 'Silver', 'Bronze', 'Total Sum']] = x[['Gold', 'Silver', 'Bronze', 'Total Sum']].astype(int)
+    
+    return x
+
+def medal_tally(df):
+    medal_tally = df.drop_duplicates(subset=['Team','NOC','Games','Year','City','Sport','Event','Medal'])
+    medal_tally = medal_tally.groupby('region').sum()[['Gold','Silver','Bronze']].sort_values('Gold',ascending=False).reset_index()
+    medal_tally['Total Sum'] = medal_tally['Gold'] + medal_tally['Silver'] + medal_tally['Bronze']
+    medal_tally[['Gold', 'Silver', 'Bronze', 'Total Sum']] = medal_tally[['Gold', 'Silver', 'Bronze', 'Total Sum']].astype(int)
+
+    return medal_tally
+
+
+def country_year_list(df):
+    years = df['Year'].unique().tolist()
+    years.sort()
+    years.insert(0,"Overall")
+
+    country = np.unique(df['region'].dropna().values).tolist()
+    country.sort()
+    country.insert(0,"Overall")
+
+    return years, country
+
+def data_over_time(df,col):
+    nations_overtime = df.drop_duplicates(['Year',col])['Year'].value_counts().reset_index().sort_values('Year')
+    nations_overtime.rename(columns={'count': col},inplace=True)
+
+    return nations_overtime
